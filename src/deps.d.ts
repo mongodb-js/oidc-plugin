73ca51ec91abb41f7fc744e31b6d15e25db6de8d
Anna Henningsen <anna@addaleax.net>
2023-05-05 17:51:48 +0200

    fix: account for ESM/CJS oddities when loading `open`

--- src/deps.d.ts
+++ src/deps.d.ts
@@ -2,3 +2,5 @@ declare module 'abort-controller' {
   export const AbortController: typeof globalThis.AbortController;
   export const AbortSignal: typeof globalThis.AbortSignal;
 }
+
+declare const __webpack_require__: unknown;
