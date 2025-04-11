3eea83ed5b3e960272587aad7a6b92ffa203d2ca
Anna Henningsen <anna@addaleax.net>
2025-04-11 16:48:42 +0200

    chore(ci): use devtools toolchain instead of nvm

--- .evergreen/use-node.sh
+++
@@ -1,8 +0,0 @@
-if [[ "$OS" == "Windows_NT" ]]; then
-  export PATH="$PWD/.deps/node/bin:$PATH"
-else
-  export NVM_DIR="$PWD/.deps/nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-fi
-
-echo "updated PATH=$PATH"
